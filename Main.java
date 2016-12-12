8f2fe5f154ec4176f51918ad5ca39912f81d8123
pretzel729 <edselalexisvalle@gmail.com>
2016-12-12 08:19:04 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+package com.pretzel.unidays.megabus;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.jsoup.Connection;
@@ -7,59 +8,77 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import static java.nio.file.StandardOpenOption.*;
 
-public class Main {
-	private static Map<String, String> cookies = new HashMap<>(), formData = new HashMap<>();
-	private static SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, dd MMM yyyy hh:mm:ss zzz");
+import static java.nio.file.StandardOpenOption.CREATE;
+import static java.nio.file.StandardOpenOption.APPEND;
 
-	private static JSONObject POST(String url) throws IOException, InterruptedException {
-		Connection.Response response = Jsoup.connect(url)
-				.followRedirects(true)
-				.ignoreContentType(true)
-				.ignoreHttpErrors(true)
-				.header("Host","access.myunidays.com")
-				.header("Connection","keep-alive")
-				.header("Accept","application/json, text/javascript, */*; q=0.01")
-				.header("Origin","https://megabus.myunidays.com")
-				.header("ud-source", "megabus")
-				.userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36")
-				.referrer("https://megabus.myunidays.com/get-perk")
-				.header("Accept-Encoding","gzip, deflate, br")
-				.header("Accept-Language","en-US,en;q=0.8")
-				.cookies(cookies)
-				.data(formData)
-				.method(Connection.Method.POST)
-				.execute();
-		cookies.putAll(response.cookies());
-		return new JSONObject(response.body());
-	}
+public class Main
+{
+	private static String FILE_NAME = "codes.txt";
 
-	public static void main(String[] args) throws InterruptedException, IOException {
-		if (args.length != 2){
-			System.out.println("Exactly 2 arguments are required: email & password");
+	//Run with
+	//java -jar ./MegabusPromo.jar 'example@utexas.edu' 'password'
+	public static void main(String[] args) throws InterruptedException, IOException
+	{
+		SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz");
+		Map<String, String> cookies = new HashMap<>();
+		Map<String, String> formData = new HashMap<>();
+
+		//Login
+		if (args.length < 2)
+		{
+			System.out.println("2 arguments are required: email & password.");
 			return;
 		}
 		formData.put("Password", args[1]);
 		formData.put("EmailAddress", args[0]);
 		formData.put("Human", "");
-		POST("https://account.myunidays.com/US/en-US/account/log-in");
-		System.out.println(cookies.containsKey("auth") ? "Login successful" : "Login unsuccessful. Wrong credentials?");
+		POST("https://account.myunidays.com/US/en-US/account/log-in", cookies, formData);
+		if (!cookies.containsKey("auth"))
+		{
+			System.out.println("Login unsuccessful. Wrong credentials?");
+			return;
+		}
+		System.out.println("Login successful!");
+
 		formData.clear();
 		formData.put("forceNew", "true");
-		for (long refreshTime; cookies.containsKey("auth"); Thread.sleep(900000 + refreshTime)) {
-			try {
-				JSONObject response = POST("https://access.myunidays.com/megabus/access/megabus/online");
+		//fetch promo code and sleep until a new one is available. Repeat
+		while (true)
+		{
+			long refreshTime = 54*60*1000;
+			try
+			{
+				JSONObject response = new JSONObject(POST("https://access.myunidays.com/megabus/access/megabus/online", cookies, formData));
 				refreshTime = dateFormat.parse(response.getString("canReissueOn")).getTime() - System.currentTimeMillis();
-				System.out.println(response.getString("code"));
-				Files.write(Paths.get("./MegabusPromoCodes.txt"), (response.getString("code") + "\n").getBytes(), CREATE, APPEND);
+				String data = dateFormat.format(new Date()) + "\t" + response.getString("code");
+				Files.write(Paths.get("./" + FILE_NAME), (data + "\n").getBytes(), CREATE, APPEND);
+				System.out.println(data);
 			}
-			catch (ParseException | JSONException | IOException e) {
+			catch (ParseException | JSONException | IOException e)
+			{
 				e.printStackTrace();
-				refreshTime = 900000;
 			}
+			Thread.sleep(refreshTime + 60*1000);
 		}
+	}
+
+	//Send an HTTP POST request
+	private static String POST(String url, Map cookies, Map formData) throws IOException, InterruptedException
+	{
+		Connection.Response response = Jsoup.connect(url)
+											.followRedirects(true)
+											.ignoreContentType(true)
+											.ignoreHttpErrors(true)
+											.header("ud-source", "megabus")
+											.cookies(cookies)
+											.data(formData)
+											.method(Connection.Method.POST)
+											.execute();
+		cookies.putAll(response.cookies());
+		return response.body();
 	}
 }
